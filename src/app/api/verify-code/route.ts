9967e5fe626c62a86037ec8330bf58e009126879
Harsh Yadav <harshyadav2027@gmail.com>
2026-07-01 20:06:10 +0530

    Add username uniqueness check and verification code handling

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,56 @@
+import dbConnect from "@/src/lib/dbConnecct";
+import UserModel from "@/src/model/User";
+import { success } from "zod";
+
+export async function POST(request :Request) {
+    await dbConnect();
+
+    try {
+        const {username , code } = await request.json();
+        const decodedUsername = decodeURIComponent(username);
+        const user = await UserModel.findOne({username: decodedUsername});
+
+        if(!user) {
+            return Response.json(
+                {success: false, message: 'User not found'},
+                {status : 404}
+            );
+        }
+
+        const isCodeValid = user.verifyCode === code;
+        const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+
+        if(isCodeValid && isCodeNotExpired){
+            user.isVerified = true;
+            await user.save();
+
+
+            return Response.json(
+                {success: true, message: 'Account verified successfully'},
+                {status: 200}
+            );
+        } else if(!isCodeNotExpired){
+            return Response.json(
+                {
+                    success: false,
+                    message:
+                        'Verification code has expired. Please sign up again too get a new code.',
+                },
+                {
+                    status: 400
+                }
+            );
+        } else {
+            return Response.json(
+                { success : false, message: 'Incoreect verification code'},
+                { status : 400 }
+            );
+        }
+    } catch (error) {
+        console.error('Error verifying user:', error);
+        return Response.json(
+            { success: false, message: 'Error verifying user'},
+            {status : 500}
+        );
+    }
+}
